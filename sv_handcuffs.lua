af08945c95fca6738028f924e0acd6dcc1f6ed15
Eser Sahiner <esershnr@gmail.com>
2017-05-02 00:01:01 +0300

    handcuff
    This is an uncomplete script.
    [UPDATABLE]

---
+++ sv_handcuffs.lua
@@ -0,0 +1,20 @@
+-- Cuff
+local handCuffed = {}
+TriggerEvent('es:addGroupCommand', 'cuff', "admin", function(source, args, user)
+		if(GetPlayerName(tonumber(args[2])))then
+			local player = tonumber(args[2])
+
+			-- User permission check
+			TriggerEvent("es:getPlayerFromId", player, function(target)
+				if(tonumber(target.permission_level) > tonumber(user.permission_level))then
+					TriggerClientEvent("chatMessage", source, "SYSTEM", {255, 0, 0}, "You're not allowed to target this person!")
+					return
+				end
+				TriggerClientEvent('mHandCuff', player, handCuffed[player])
+			end)
+		else
+			TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, "Incorrect player ID!")
+		end
+end, function(source, args, user)
+	TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, "Insufficienct permissions!")
+end)
